a76e8330837ea303a2821f2b5e7b13dd1cf74a62
Trent Mick <trent.mick@elastic.co>
2024-03-11 17:33:01 -0700

    Add a breaking mapped-exports test case
    This builds on https://github.com/elastic/require-in-the-middle/pull/82
    This first commit is a breaking test case to discuss.

--- test/mapped-exports.js
+++ test/mapped-exports.js
@@ -3,7 +3,7 @@
 const test = require('tape')
 const { Hook } = require('../')
 
-test('handles mapped exports', function (t) {
+test('handles mapped exports: mapped-exports/foo', function (t) {
   t.plan(2)
 
   const hook = new Hook(['mapped-exports/foo'], function (exports, name) {
@@ -15,8 +15,26 @@ test('handles mapped exports', function (t) {
     return exports
   })
 
-  t.on('end', hook.unhook)
-
   const foo = require('mapped-exports/foo')
   t.equal(foo.answer(), 'wrapped-42')
+
+  hook.unhook()
+})
+
+test('handles mapped exports: mapped-exports/bar', function (t) {
+  t.plan(2)
+
+  const hook = new Hook(['mapped-exports/bar'], function (exports, name) {
+    t.equal(name, 'mapped-exports/bar')
+    const answer = exports.answer
+    exports.answer = function wrappedAnswer () {
+      return 'wrapped-' + answer()
+    }
+    return exports
+  })
+
+  const bar = require('mapped-exports/bar')
+  t.equal(bar.answer(), 'wrapped-42')
+
+  hook.unhook()
 })
